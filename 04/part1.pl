c0316900674b6dbcc9665e7db7b3e5246bdd2547
Matthew Horsfall <wolfsage@gmail.com>
2022-12-10 16:58:56 -0500

    Add 2022 advent of code day 4 solvers

---
+++ 04/part1.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use experimental qw(signatures);
+use feature qw(state);
+
+my $overlaps = 0;
+
+while (my $line = <<>>) {
+  my ($e1, $e2) = split(',', $line);
+
+  my ($min1, $max1) = split('-', $e1);
+  my ($min2, $max2) = split('-', $e2);
+
+  if ($min1 >= $min2 && $max1 <= $max2) {
+    $overlaps++;
+  } elsif ($min2 >= $min1 && $max2 <= $max1) {
+    $overlaps++;
+  }
+}
+
+print "Overlaps: $overlaps\n";
